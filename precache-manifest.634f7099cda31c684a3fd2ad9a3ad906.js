d87891d6b42fa03f93140378a5297605426d171d
jnannni <catya.ladokhina@gmail.com>
2020-05-26 12:08:39 +0300

    deploy

---
+++ precache-manifest.634f7099cda31c684a3fd2ad9a3ad906.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6e6079141734e7da9cbc52d658fa05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e51a1d9440d1a1b2f34",
+    "url": "/static/css/main.65dc0727.chunk.css"
+  },
+  {
+    "revision": "bc33587ab0c6352c4f59",
+    "url": "/static/js/2.31feb1a6.chunk.js"
+  },
+  {
+    "revision": "9d0c2ea35611e9538ed0b4599a92999a",
+    "url": "/static/js/2.31feb1a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e51a1d9440d1a1b2f34",
+    "url": "/static/js/main.b3fb7f29.chunk.js"
+  },
+  {
+    "revision": "8b8f6b318cc74ffe781f",
+    "url": "/static/js/runtime-main.6468f01a.js"
+  },
+  {
+    "revision": "562012c692825f23bcbbd0030bf9e7b5",
+    "url": "/static/media/background.562012c6.jpg"
+  }
+]);
